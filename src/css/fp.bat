d9ee7b912a720b46bcf615fa0e9ac93e211a5474
iahu <iahu1988@gmail.com>
2013-11-15 16:07:53 +0800

    调整文件

--- src/css/fp.bat
+++ src/css/fp.bat
@@ -13,12 +13,12 @@ echo /* %name%.css */>%name%.css
 echo ^<!doctype html^>>test_%name%.html
 echo ^<html lang="en"^>>>test_%name%.html
 echo ^<head^>>>test_%name%.html
-echo    <meta charset="UTF-8"^>>>test_%name%.html
+echo    ^<meta charset^="UTF-8"^>>>test_%name%.html
 echo    ^<title^>test %name%^</title^>>>test_%name%.html
-echo    ^<link rel="stylesheet" href="%name%.css"^>>>test_%name%.html
+echo    ^<link rel^="stylesheet" href^="%name%.css"^>>>test_%name%.html
 echo    ^<style^>>>test_%name%.html
 echo    body {padding: 0;margin: 0;}>>test_%name%.html
-echo    #about {position: absolute;left: 0;top: 0;width: 20em;padding: 0 12px;background-color: #ffe;height: 100%;border-right: 1px solid #eea;}>>test_%name%.html
+echo    #about {position: absolute;left: 0;top: 0;width: 20em;padding: 0 12px;background-color: #ffe;height: 100%%;border-right: 1px solid #eea;}>>test_%name%.html
 echo    #about .about-des {margin-bottom: 50px;color: #37A4A8;}>>test_%name%.html
 echo    #about h2, #demo h2 {color:#399;}>>test_%name%.html
 echo    #demo {position: relative;margin-left: 24em;padding: 0 40px 0 12px;}>>test_%name%.html
@@ -33,11 +33,13 @@ echo    ^<div id="about"^>>>test_%name%.html
 echo        ^<h2^>ËµÃ÷^</h2^>>>test_%name%.html
 echo        ^<div class="about-des"^>>>test_%name%.html
 echo        ^</div^>>>test_%name%.html
-echo >>test_%name%.html
+echo. >>test_%name%.html
 echo        html Æ¬¶Î£º>>test_%name%.html
 echo        ^<pre class="code"^>>>test_%name%.html
 echo        ^</pre^>>>test_%name%.html
 echo    ^</div^>>>test_%name%.html
+echo. >>test_%name%.html
+echo. >>test_%name%.html
 echo    ^<div id="demo"^>>>test_%name%.html
 echo        ^<h2^>DEMO^</h2^>>>test_%name%.html
 echo    ^</div^>>>test_%name%.html
